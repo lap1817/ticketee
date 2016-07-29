c2ecd2461b88474cd35edc485d9ea39b5f812589
Feng Pan <seraph_8014@hotmail.com>
2016-07-29 00:07:42 -0700

    add validation to the create project feature

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Users can create new projects" do
-	scenario "with valid attribute" do
+	before do
 		visit "/"
 		click_link "New Project"
+		
+	end
+
+	scenario "with valid attribute" do
 		fill_in "Name", with: "Sublime test 3"
 		fill_in "Description", with: "a text editor for everyone"
 		click_button "Create Project"
@@ -13,5 +17,12 @@ RSpec.feature "Users can create new projects" do
 		expect(page.current_url).to eq project_url(project)
 		title = "Sublime test 3 - Projects - Ticketee"
 		expect(page).to have_title title
+	end
+	
+	scenario "when providing invalid attributes" do
+		click_button "Create Project"
+		
+		expect(page).to have_content "Project has not been created"
+		expect(page).to have_content "Name can't be blank"
 	end
 end
