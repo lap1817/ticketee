b2c07a5d25b5d4a48842a463d2b5f7912e9fe3bf
Feng Pan <seraph_8014@hotmail.com>
2016-07-28 23:38:45 -0700

    enable title on the show project page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -4,9 +4,14 @@ RSpec.feature "Users can create new projects" do
 	scenario "with valid attribute" do
 		visit "/"
 		click_link "New Project"
-		fill_in "Name", with: "Sublime teset 3"
+		fill_in "Name", with: "Sublime test 3"
 		fill_in "Description", with: "a text editor for everyone"
 		click_button "Create Project"
 		expect(page).to have_content "Project has been created"
+		
+		project = Project.find_by(name: "Sublime test 3")
+		expect(page.current_url).to eq project_url(project)
+		title = "Sublime test 3 - Projects - Ticketee"
+		expect(page).to have_title title
 	end
 end
