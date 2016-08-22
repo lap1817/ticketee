d8f185195862fe2ce9d68aefeea055d8e5805ecf
Feng Pan <seraph_8014@hotmail.com>
2016-08-22 00:00:23 -0700

    rescue from unauthrozied error when viewing project

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,6 +4,9 @@ class Admin::ApplicationController < ApplicationController
   def index
   end
   
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  
   private 
 	def authorize_user!
 		authenticate_user!
@@ -12,4 +15,9 @@ class Admin::ApplicationController < ApplicationController
 			redirect_to root_path, alert: "You must be an admin to do that."
 		end
 	end
+	
+	def not_authorized
+		redirect_to root_path, alert: "You aren't allowed to do that."
+	end
+
 end
